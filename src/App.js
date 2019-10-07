02444278f3919797a8efc2b201bbf93b57c42254
Vinicius Eduardo Trindade Antonio <vinicius.trindade@castinformatica.onmicrosoft.com>
2019-10-07 17:59:05 -0300

    commit inicial

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Navigation from "./components/navigation/Navigation";
+import Navigation from "./components/logo/Logo";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Navigation />
+      <Logo />
+    </div>
+  );
+}
+
+export default App;
