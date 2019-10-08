89ebb5cbf67838e0e1bce05533ed4b7706289058
Vinicius Trindade <vinicius.trincid@gmail.com>
2019-10-07 21:41:43 -0300

    terminando component logo

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Navigation from "./components/navigation/Navigation";
-import Navigation from "./components/logo/Logo";
+import Logo from "./components/logo/Logo";
 import "./App.css";
 
 function App() {
